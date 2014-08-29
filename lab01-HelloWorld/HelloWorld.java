9f17e9852be69b4393f3292b3afceb52a62413cd
wcn217 <wcn217@lehigh.edu>
2014-08-29 13:26:33 +0000

    updated HelloWorld

--- lab01-HelloWorld/HelloWorld.java
+++ lab01-HelloWorld/HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("####################");
+        System.out.println("####################");
+        System.out.println("#                  #");
+        System.out.println("# Hello World 2014 #");
+        System.out.println("#                  #");
+        System.out.println("####################");
+        System.out.println("####################");
         System.out.println("Test");
     }
 }
