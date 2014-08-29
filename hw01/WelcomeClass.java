accacf292ba49f9d1579a06bb8fac8d586bfc0fe
wcn217 <wcn217@lehigh.edu>
2014-08-29 02:05:10 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,12 @@
+public class WelcomeClass{
+    public static void main(String[] args){
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\ / \\ / \\ / \\ / \\ ");
+        System.out.println("|-W--C--N--2--1--7-|");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println(" v   v   v   v   v   ");
+    }
+}
