3b5fa2d6223d3cad6f3d74a3ef9dea4b7cceb504
wcn217 <wcn217@lehigh.edu>
2014-08-29 09:32:12 -0400

    Update WelcomeClass.java

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -8,5 +8,6 @@ public class WelcomeClass{
         System.out.println("|-W--C--N--2--1--7-|");
         System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
         System.out.println(" v   v   v   v   v   ");
+        //A \ will not print unless you add a second one next to it.
     }
 }
